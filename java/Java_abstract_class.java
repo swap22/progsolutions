5e13d4701a312d2f340b62e51f4029642690784e
swap22 <sharma.sswapnil@gmail.com>
2017-04-01 17:24:12 +0530

    new solve

---
+++ java/Java_abstract_class.java
@@ -0,0 +1,31 @@
+//https://www.hackerrank.com/challenges/java-abstract-class
+
+
+import java.util.*;
+
+abstract class Book {
+    String title;
+    abstract void setTitle(String s);
+    String getTitle() {
+        return title;
+    }
+}
+
+class MyBook extends Book {
+    @Override   // writing before method will help compilers debug any issues with inheritance.
+    void setTitle(String s) {
+        title = s;
+    }
+}
+    
+public class Main {
+    public static void main(String []args) {
+        //Book new_novel=new Book(); This line prHMain.java:25: error: Book is abstract; cannot be instantiated
+        Scanner sc = new Scanner(System.in);
+        String title = sc.nextLine();
+        MyBook new_novel = new MyBook();
+        new_novel.setTitle(title);
+        System.out.println("The title is: "+ new_novel.getTitle());
+        sc.close();
+    }
+}
